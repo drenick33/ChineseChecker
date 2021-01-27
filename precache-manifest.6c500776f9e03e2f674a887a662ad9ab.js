d7ad7c5ea11b0a29e62d55cb8e9f055071d47595
drenick33 <drenick1@binghamton.edu>
2021-01-27 17:36:24 -0600

    Updates

---
+++ precache-manifest.6c500776f9e03e2f674a887a662ad9ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1940f4b1d78a7bc54e7ccb3c97ca5117",
+    "url": "/ChineseChecker/index.html"
+  },
+  {
+    "revision": "9ed91115aef39733167a",
+    "url": "/ChineseChecker/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "c3476292314aeaf73806",
+    "url": "/ChineseChecker/static/css/main.981275f7.chunk.css"
+  },
+  {
+    "revision": "9ed91115aef39733167a",
+    "url": "/ChineseChecker/static/js/2.287e2ce8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ChineseChecker/static/js/2.287e2ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3476292314aeaf73806",
+    "url": "/ChineseChecker/static/js/main.d14367f2.chunk.js"
+  },
+  {
+    "revision": "2d48ec6ca7c0e4e116ec",
+    "url": "/ChineseChecker/static/js/runtime-main.71e8cda8.js"
+  }
+]);
