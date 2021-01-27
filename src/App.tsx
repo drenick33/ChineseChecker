832be008388a8a0dd9afbe71476b9fa1c82be5d1
drenick33 <drenick1@binghamton.edu>
2021-01-27 17:40:55 -0600

    removed router to make single page app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.less';
 import Navbar from './components/layout/Navbar';
 import Main from './components/pages/main/Main';
@@ -9,16 +8,12 @@ const { Header } = Layout;
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Header>
-          <Navbar></Navbar>
-        </Header>
-        <Switch>
-          <Route exact path='/' component={Main} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className='App'>
+      <Header>
+        <Navbar></Navbar>
+      </Header>
+      <Main />
+    </div>
   );
 }
 
